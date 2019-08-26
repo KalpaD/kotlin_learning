be3c52bd6f3ac3519301db98744159dded4042e1
Kalpa Senanayake <senanayake.kalpa@gmail.com>
2019-08-26 21:33:32 +1000

    lesson nullability

---
+++ src/nullabletypes/TypeCase.kt
@@ -0,0 +1,30 @@
+package nullabletypes
+
+
+fun main() {
+
+}
+
+
+// type cast
+
+// if (any is String) {
+    //val s = any as String
+    //s.toUpperCase()
+//}
+
+// we do not have to explicitly cast is like 'any as String'
+// we can use the original variable
+
+// if (any is String) {
+//      any.toUpperCase()
+// }
+
+// safe cast
+
+// if (any is String) {
+//  any.toUpperCase()
+//}
+
+// can be expressed as
+// (any as? String)?.toUpperCase()
