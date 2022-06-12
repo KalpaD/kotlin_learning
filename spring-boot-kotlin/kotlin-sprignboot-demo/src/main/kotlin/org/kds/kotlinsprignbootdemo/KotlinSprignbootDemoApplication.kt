7b149d4ede353cc9ae9d7b7548f4d776471255d3
Kalpa Senanayake <senanayake.kalpa@gmail.com>
2022-06-12 15:29:39 +1000

    Adding initial structure of the spring boot project

---
+++ spring-boot-kotlin/kotlin-sprignboot-demo/src/main/kotlin/org/kds/kotlinsprignbootdemo/KotlinSprignbootDemoApplication.kt
@@ -0,0 +1,11 @@
+package org.kds.kotlinsprignbootdemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinSprignbootDemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinSprignbootDemoApplication>(*args)
+}
---
+++ spring-boot-kotlin/kotlin-sprignboot-demo/src/test/kotlin/org/kds/kotlinsprignbootdemo/KotlinSprignbootDemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.kds.kotlinsprignbootdemo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinSprignbootDemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
